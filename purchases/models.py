2c9d9b2719450cb11ebacc26785eef7d85484b5f
Dennys Solis <dennyssolis@gmail.com>
2020-01-21 22:16:19 -0500

    Ending Product (CRUD)

---
+++ purchases/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from inventory.models import ClassModel
+
+class Vendor(ClassModel):
+    description=models.CharField(
+        max_length=100,
+        unique=True
+        )
+    address=models.CharField(
+        max_length=250,
+        null=True, blank=True
+        )
+    contact=models.CharField(
+        max_length=100
+    )
+    phone=models.CharField(
+        max_length=10,
+        null=True, blank=True
+    )
+    email=models.CharField(
+        max_length=250,
+        null=True, blank=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.description)
+
+    def save(self):
+        self.description = self.description.upper()
+        super(Vendor, self).save()
+
+    class Meta:
+        verbose_name_plural = "Vendors"
