79b70dae69296819cf69693e756d4a25bbbac8c6
Dennys <dennyssolis@gmail.com>
2020-02-28 13:46:07 -0500

    Cleaning templates

--- purchases/views.py
+++ purchases/views.py
@@ -1,22 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+
+from bases.views import NotPrivileges
 
 
 import json 
 from .forms import VendorForm
 from .models import Vendor
 
-class VendorView(LoginRequiredMixin, ListView):
+class VendorView(NotPrivileges, ListView):
+    permission_required = 'purchases.view_vendor'
     model =  Vendor
     template_name = 'purchases/vendor_list.html'
     context_object_name =  'obj'
     login_url = reverse_lazy('bases:login')
 
 
-class VendorNew(LoginRequiredMixin, CreateView):
+class VendorNew(NotPrivileges, CreateView):
+    permission_required = 'purchases.add_vendor'
     model = Vendor
     template_name = 'purchases/vendor_form.html'
     form_class = VendorForm
@@ -27,7 +32,8 @@ class VendorNew(LoginRequiredMixin, CreateView):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
 
-class VendorUpdate(LoginRequiredMixin, UpdateView):
+class VendorUpdate(NotPrivileges, UpdateView):
+    permission_required = 'purchases.change_vendor'
     model = Vendor
     template_name = 'purchases/vendor_form.html'
     form_class = VendorForm
@@ -39,6 +45,8 @@ class VendorUpdate(LoginRequiredMixin, UpdateView):
         form.instance.updated_by = self.request.user
         return super().form_valid(form)
 
+@login_required(login_url='/login/')
+@permission_required('purchases.change_vendor', login_url='bases:home_not_privileges')
 def vendor_inactivate(request, pk):
     template_name = 'purchases/inactivate_vendor.html'
     obj = Vendor.objects.filter(pk=pk).first()
